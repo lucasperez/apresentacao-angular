ef8bfb12a579587b3b09a9861eca75212e38486e
Lucas Pérez <lucas.perez@ideais.com.br>
2014-01-14 16:08:10 -0200

    criando datepicker

--- exemplos-codigo/crud/crud.js
+++ exemplos-codigo/crud/crud.js
@@ -29,3 +29,18 @@ function Colaborador(args){
 	this.nome = args.nome || "";
 	this.data = args.data || "";	
 }
+
+crudModule.directive('datepicker', function() {
+  return function(scope, element, attrs) {
+    element.datepicker({
+      dateFormat: 'dd/mm/yy',
+      constrainInput: true,   
+      showAnim : 'clip',
+      showSpeed : 'slow',
+      showWeeks: true,
+      onSelect : function(dateText, elem){
+        $(this).trigger('input');
+      }
+    });
+   }
+});
